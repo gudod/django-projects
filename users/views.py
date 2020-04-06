96d9dc1414184298d4aed7e9bc18d3beeea61c14
Davison, Gudo <gudo.davison@gmail.com>
2020-04-06 09:26:09 +0200

    solving redirect

--- users/views.py
+++ users/views.py
@@ -7,9 +7,10 @@ def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
+            form.save()
             username=form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
-            return redirect('http://127.0.0.1:8000/')
+            return redirect('/')
     else:
         form=UserCreationForm()
     return render(request, 'users/register.html', {'form': form})
